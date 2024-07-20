c9607bd35b445aba9efcb677f5875bb15c345213
mahdi <shm703586@gmail.com>
2024-07-20 18:38:11 +0330

    update

---
+++ website/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from website.views import *
+urlpatterns = [
+    path('http-test',http_test),
+]
---
+++ website/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def http_test(request):
+    return HttpResponse('HELLO')
+
